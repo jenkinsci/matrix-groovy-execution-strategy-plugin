05482b771abe358a1be90a1a0e3f16e123865371
JeremyMarshall <jeremystuartmarshall@gmail.com>
2015-10-11 12:10:09 +1100

    fixed listener

--- src/main/groovy/org/jenkinsci/plugins/GroovyScriptMES.groovy
+++ src/main/groovy/org/jenkinsci/plugins/GroovyScriptMES.groovy
@@ -1,49 +1,96 @@
 package org.jenkinsci.plugins
 
 import hudson.Extension
+import hudson.model.Descriptor
+import jenkins.model.Jenkins
 import org.kohsuke.stapler.DataBoundConstructor
 import hudson.matrix.Combination
 import hudson.matrix.MatrixBuild
 import hudson.matrix.MatrixBuild.MatrixBuildExecution
 import hudson.matrix.MatrixExecutionStrategyDescriptor
-
+import org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript
+import org.jenkinsci.plugins.scriptsecurity.scripts.ApprovalContext
+import org.jenkinsci.plugins.scriptsecurity.scripts.ClasspathEntry
+import net.sf.json.JSONObject
+import org.kohsuke.stapler.StaplerRequest
+import hudson.util.FormValidation
 /**
  * Created by jeremymarshall on 12/10/2014.
  */
 class GroovyScriptMES extends BaseMES {
 
-    String script
+    SecureGroovyScript secureScript
+
+    transient String script
     String scriptFile
     String scriptType = 'script'
 
     @DataBoundConstructor
-    GroovyScriptMES( String script, String scriptFile, String scriptType) {
-        this.script = script
+    GroovyScriptMES( SecureGroovyScript secureScript, String scriptFile, String scriptType) {
+        this.secureScript = secureScript
         this.scriptFile = scriptFile
         this.scriptType = scriptType
+
+        this.secureScript.configuringWithKeyItem()
     }
 
     Map decideOrder(MatrixBuild.MatrixBuildExecution execution, List<Combination> comb) {
 
-        def scriptRunner
-        def defaultScript = """\
-            result['default'] = combinations
-            result
-        """
+        ScriptRunner scriptRunner
+        SecureGroovyScript myScript
 
         if (scriptType == 'script' || scriptType == '') {
-            scriptRunner = new ScriptRunner(execution, new StringReader(script ?: defaultScript))
+            if(secureScript.script != "") {
+                myScript = secureScript
+            } else {
+                def result = [:]
+                result['default'] = comb
+                return result
+            }
+        } else if(! Jenkins.instance.getDescriptor(this.class).secureOnly) {
+            List<ClasspathEntry> cp = new ArrayList<ClasspathEntry>()
+
+            def scriptInFile = new WorkspaceFileReader(scriptFile).scriptFile.text
+            myScript = new SecureGroovyScript(scriptInFile, false, cp).configuring(ApprovalContext.create());
+            myScript.configuringWithKeyItem()
         } else {
-            scriptRunner = new ScriptRunner(execution, new WorkspaceFileReader(scriptFile).scriptFile)
+            throw new Exception("File based matrix groovy execution script not allowed")
         }
 
+        scriptRunner = new ScriptRunner(execution, myScript)
         def ret = scriptRunner.run(comb)
 
         ret
     }
 
+    private Object readResolve() {
+        if (script != null) {
+            List<ClasspathEntry> cp = new ArrayList<ClasspathEntry>();
+
+            secureScript = new SecureGroovyScript(script, false, cp).configuring(ApprovalContext.create());
+            script = null;
+        }
+        return this;
+    }
+
+
     @Extension
     static class DescriptorImpl extends MatrixExecutionStrategyDescriptor {
         final String displayName = 'Groovy Script Matrix Executor Strategy'
+
+        boolean secureOnly = false
+
+        public DescriptorImpl(){
+            load()
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject formData) throws Descriptor.FormException {
+            // To persist global configuration information,
+            // set that to properties and call save().
+            req.bindJSON(this, formData)
+            save()
+            true
+        }
     }
 }
