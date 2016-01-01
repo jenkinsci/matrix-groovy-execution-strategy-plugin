3759b2d5b40c0dc3447ff611fd8dff4eeb19ce61
JeremyMarshall <jeremystuartmarshall@gmail.com>
2016-01-01 22:20:02 +1100

    fix environment binding

--- src/main/groovy/org/jenkinsci/plugins/ScriptRunner.groovy
+++ src/main/groovy/org/jenkinsci/plugins/ScriptRunner.groovy
@@ -7,6 +7,7 @@ import hudson.matrix.MatrixBuild
 import hudson.matrix.MatrixBuild.MatrixBuildExecution
 import org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript
 import hudson.model.ParametersAction
+import org.jenkinsci.plugins.envinject.EnvInjectPluginAction
 
 /**
  * Created by jeremymarshall on 10/10/2014.
@@ -36,23 +37,36 @@ class ScriptRunner {
         binding.setVariable('result', new TreeMap<String, List<Combination>>())
         binding.setVariable('out', this.execution.listener.logger)
 
-
         def parameters = [:]
         def build = this.execution.build
         def resolver = build.buildVariableResolver
 
         def p = build?.actions.find{ it instanceof ParametersAction }?.parameters
         p.each {
-
             def param_value = resolver.resolve(it.name)
 
             parameters[it.name] = param_value.value
-            //println "parameter ${it.name}:"
-            //println it.dump()
+        }
+        binding.setVariable('parameters', parameters)
+
+        //only one of these at most
+        //only if envinject is here and it is in use
+        //otherwise use the environment
+        def env = [:]
+
+        if (Jenkins.instance.getPlugin('envinject')) {
+            def e = build?.actions.find { it instanceof EnvInjectPluginAction }?.envMap
+
+            if (e) {
+                env = e
+            } else {
+                env = System.getenv()
+            }
+        } else {
+            env = System.getenv()
         }
 
-        binding.setVariable('parameters', parameters)
-        binding.setVariable('env', System.getenv())
+        binding.setVariable('env', env )
 
         def res = script.evaluate(getClass().classLoader, binding)
         res
