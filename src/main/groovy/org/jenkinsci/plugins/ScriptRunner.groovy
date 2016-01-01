dd53f81665fd41ea79ab98773ffafb3a43270875
JeremyMarshall <jeremystuartmarshall@gmail.com>
2016-01-01 19:34:11 +1100

    add parameters in to script

--- src/main/groovy/org/jenkinsci/plugins/ScriptRunner.groovy
+++ src/main/groovy/org/jenkinsci/plugins/ScriptRunner.groovy
@@ -6,6 +6,7 @@ import hudson.model.OneOffExecutor
 import hudson.matrix.MatrixBuild
 import hudson.matrix.MatrixBuild.MatrixBuildExecution
 import org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript
+import hudson.model.ParametersAction
 
 /**
  * Created by jeremymarshall on 10/10/2014.
@@ -33,6 +34,25 @@ class ScriptRunner {
         binding.setVariable('workspace', this.workspace)
         binding.setVariable('execution', this.execution)
         binding.setVariable('result', new TreeMap<String, List<Combination>>())
+        binding.setVariable('out', this.execution.listener.logger)
+
+
+        def parameters = [:]
+        def build = this.execution.build
+        def resolver = build.buildVariableResolver
+
+        def p = build?.actions.find{ it instanceof ParametersAction }?.parameters
+        p.each {
+
+            def param_value = resolver.resolve(it.name)
+
+            parameters[it.name] = param_value.value
+            //println "parameter ${it.name}:"
+            //println it.dump()
+        }
+
+        binding.setVariable('parameters', parameters)
+        binding.setVariable('env', System.getenv())
 
         def res = script.evaluate(getClass().classLoader, binding)
         res
