26f83a0f3728696ee60c3e6b7cb9caab77920362
Jeremy Marshall <JeremyMarshall@users.noreply.github.com>
2017-07-09 21:18:34 +1000

    Groovy3.2.1 (#14)
    * broken tests
    
    * not fixed
    
    * fixed for latest gradle-jpi

--- src/test/groovy/org/jenkinsci/plugins/GroovyScriptMESSpec.groovy
+++ src/test/groovy/org/jenkinsci/plugins/GroovyScriptMESSpec.groovy
@@ -6,20 +6,24 @@ import hudson.matrix.MatrixProject
 import hudson.matrix.TextAxis
 import hudson.model.AbstractBuild
 import hudson.model.BuildListener
+import org.junit.ClassRule
+import org.jvnet.hudson.test.JenkinsRule
 import org.jvnet.hudson.test.TestBuilder
+import spock.lang.Shared
 import spock.lang.Specification
 import org.junit.Rule
-import org.jvnet.hudson.test.GroovyJenkinsRule
+
 import org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript
 
 class GroovyScriptMESSpec extends Specification {
 
+    //@Shared
+    //@ClassRule
     @Rule
-    GroovyJenkinsRule rule = new GroovyJenkinsRule()
+    JenkinsRule rule = new JenkinsRule()
 
     MatrixProject configure(Boolean failAX=false) {
-
-        def matrixProject = rule.createMatrixProject()
+        def matrixProject = rule.jenkins.createProject(MatrixProject, 'test1');
 
         def axis = new TextAxis('axis1', ['a', 'b', 'c'])
         def axis2 = new TextAxis('axis2', ['x', 'y', 'z'])
